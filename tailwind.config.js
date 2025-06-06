3235f27955ffaa3423119d0eca8e777b9bb95839
Farah Bassoumi <bassoumiifarah@gmail.com>
2025-06-07 00:26:52 +0100

    revoke tertiary color value in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
     extend: {   colors: {
       primary: "#050816",
       secondary: "#bfa6c3",
-      tertiary: "#301025",
+      tertiary: "#151030",
+
       "black-100": "#100d25",
       "black-200": "#090325",
       "white-100": "#f3f3f3",
